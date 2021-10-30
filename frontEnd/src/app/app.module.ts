4bcf0f9502d031dc435ff428fb60d80fd6aa9c39
Jessie-brenes <jessiejosuebrenesruiz@gmail.com>
2021-10-29 21:23:25 -0600

    backend listo

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -7,11 +7,11 @@ import {HttpClientModule } from '@angular/common/http';
 import {RouterModule, Routes } from '@angular/router'; 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 
 import {MatCardModule} from '@angular/material/card';
 
-import { LoginComponent } from './login/login.component';
 
 const appRoutes: Routes = [
   {
